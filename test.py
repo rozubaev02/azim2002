c33e0abe6cf3a9bee0d09e154909b0411b8b2f14
rozubaev02 <rozubaev.azim@mail.ru>
2021-05-03 20:56:44 +0600

    initial commit

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-print('Salamu aleikum')
-print('aslan')
-print('forms')
+# print('Salamu aleikum')
+# print('aslan')
+# print('forms')
+
+lst = ['Nurkyz', 'Adliet', 'Abai', 'Siymyk', 'Ulan', 'Adilet', 'Richard', 'Insan', 'Azim']
+for i in lst:
+    print(i[0])
